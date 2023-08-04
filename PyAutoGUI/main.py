0ad4a3523ab7ae7754ce3afb207d280c2946d5fe
konglei <konglei@thinktech.net.cn>
2023-08-04 14:25:03 +0800

    update

--- PyAutoGUI/main.py
+++ PyAutoGUI/main.py
@@ -6,10 +6,9 @@ if __name__ == "__main__":
         # while 1:
         #     print(pyautogui.position())
         time.sleep(1.5)
-        pyautogui.scroll(-700)
+        pyautogui.scroll(-500)
         time.sleep(0.5)
-        x = pyautogui.locateOnScreen(
-            'a8s.png', region=(450, 300, 200, 1700))
+        x = pyautogui.locateOnScreen('a8.png', region=(800, 300, 200, 1700))
         if (x):
             pyautogui.click(x.left, x.top, duration=0.3)
             time.sleep(0.2)
--- PySerial/main.py
+++ PySerial/main.py
@@ -2,46 +2,63 @@ import serial
 import serial.tools.list_ports
 from time import sleep
 from pprint import pprint
-import time
-import binascii
+import threading
 
-for port in list(serial.tools.list_ports.comports()):
-    pprint(port.device)
+COM_IN = None
+COM_OUT = None
 
-def recive():
-    ser = serial.Serial("COM44", 115200)
+ports = list(serial.tools.list_ports.comports())
+if ports.__len__() >= 2:
+    COM_IN = ports[0].device
+    COM_OUT = ports[1].device
+
+def print_ports():
+    for port in list(serial.tools.list_ports.comports()):
+        pprint(port.device)
+
+file_r = "FLS.xmind"
+file_w = "FLS1.xmind"
+# file_r = "datar.txt"
+# file_w = "dataw.txt"
+
+def uart_recive():
+    ser = serial.Serial(COM_IN, 115200, timeout = 3)
     print("port state : ", ser.is_open)
-    while True:
-        print(ser.readline().decode('utf-8'))
+    ser.write("ready\n".encode('utf-8'))
 
-def trans():
-    ser = serial.Serial("COM48", 115200)
+    # while True:
+    #     pass
+
+
+    # with open(file_w, "wb") as fw:
+    #     content = ser.read(0x100)
+    #     while content:
+    #         if len(content) != 0x100:
+    #             print(len(content))
+    #         fw.write(content)
+    #         content = ser.read(0x100)
+    print("uart_recive over")
+
+def uart_trans():
+    ser = serial.Serial(COM_OUT, 115200, timeout = 3)
     print("port state : ", ser.is_open)
-    string = '''0123456789ABCDEF'''+'\n'
-    print(string.encode('utf-8').__len__())
-    while True:
-        time.sleep(1)
-        ser.write(string.encode('utf-8'))
 
-def bin_to_hex():
-    # filename = "C:\\GIT\\Github\\python\\PySerial\\FLS.xmind"
-    filename = "C:\\GIT\\Github\\python\\PySerial\\data.txt"
-    content = "0"
-    with open(filename, "rb") as f:
-        content  = f.read().hex()
-    array = []
-    for index in range(0, len(content), 2):
-        array.append(content[index: index+2])
+    print(ser.read())
 
-def hex_to_bin(hex_str):
-    array = []
-    for index in range(0, len(hex_str), 2):
-        array.append(hex_str[index: index+2])
-    return bytes(int(x, 16) for x in array)
+    # while  != "ready\n":
+    #     pass
 
-# trans()
-filename1 = "C:\\GIT\\Github\\python\\PySerial\\dataw.txt"
-with open(filename1, "wb") as f:
-    bin_str = hex_to_bin("31323334353637383930")
-    print(bin_str)
-    f.write(bin_str)
+    # with open(file_r, "rb") as fr:
+    #     content = fr.read(0x100)
+    #     while content:
+    #         if len(content) != 0x100:
+    #             print(len(content))
+    #         ser.write(content)
+    #         content = fr.read(0x100)
+    print("uart_trans over")
+
+print_ports()
+
+threading.Thread(target=uart_recive).start()
+# sleep(0.5)
+threading.Thread(target=uart_trans).start()
